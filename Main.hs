7febfec33c342dcf5c6463c33c1e86a2bf9e276c
Luke Iannini <lukexi@me.com>
2015-06-08 01:47:25 -0700

    Add client threads

--- Main.hs
+++ Main.hs
@@ -7,30 +7,71 @@ import Data.ByteString (ByteString)
 import Control.Monad.Trans
 import Control.Concurrent
 import Control.Monad
+import           Data.Map (Map)
+import qualified Data.Map as Map
+
+type ClientMap = Map SockAddr (Chan ByteString)
 
 threadDelaySec :: Int -> IO ()
 threadDelaySec = threadDelay . (*1000000)
 
+launchClientChannel :: MVar ClientMap -> SockAddr -> IO (Chan ByteString)
+launchClientChannel clientsMVar clientSockAddr = do
+
+  (Just hostName, Just serviceName) <- getNameInfo [] True True clientSockAddr
+  let displayName = show hostName ++ " : " ++ show serviceName
+  clientChannel <- newChan
+
+  putStrLn $ "Launching client channel to " ++ displayName
+  
+  forkIO $ do 
+    toClientSock <- socketToAddress hostName clientPort
+
+    let removeClient = do
+          putStrLn $ "Removing client " ++ displayName
+          sClose toClientSock
+          withMVar clientsMVar $ return . Map.delete clientSockAddr
+
+    (`finally` removeClient) . forever $ do
+      message <- readChan clientChannel
+      putStrLn $ "Server Broadcasting " ++ (decode' message) ++ " to " ++ displayName
+      send toClientSock message
+  return clientChannel
+
 launchServer :: IO ()
 launchServer = void . forkIO $ do
   serverSock <- listenSocket serverPort
+
+  clientsMVar <- newMVar Map.empty
 
   putStrLn "i'm a server"
 
   forever $ do
     (stuff, fromAddr) <- recvFrom serverSock 4096
     let got = decode' stuff :: String
-    putStrLn $ "got stuff: " ++ show got ++ " from " ++ show fromAddr
+    putStrLn $ "Server got: " ++ show got ++ " from " ++ show fromAddr
 
-  
+    withMVar clientsMVar $ \clients -> do
+      newClients <- case Map.lookup fromAddr clients of
+        Just _ -> return clients
+        Nothing            -> do
+          clientChannel <- launchClientChannel clientsMVar fromAddr 
+          return $ Map.insert fromAddr clientChannel clients
+      forM newClients $ \clientChannel -> 
+        writeChan clientChannel stuff
+      return newClients
 
 launchClient :: IO ThreadId
-launchClient = forkIO $ asClient $ \clientSock -> do
-  -- clientSock <- socketToAddress serverAddr serverPort
+launchClient = forkIO $ asClient $ \sendSock -> do
 
-  sendB clientSock ""
+  sendB sendSock "hiiiii"
+
+  receiveSock <- listenSocket clientPort
 
   putStrLn "I'm a client"
+  response <- recv receiveSock 4096
+  let got = decode' response :: String
+  putStrLn $ "Client got " ++ show got
 
 main :: IO ()
 main = do
@@ -47,13 +88,16 @@ main = do
 
   threadDelaySec 1
   putStrLn "Launching another client..."
-  client <- launchClient
+  _client2 <- launchClient
 
   threadDelaySec 1
   putStrLn "Done."
 
 serverPort :: String
 serverPort = "3000"
+
+clientPort :: String
+clientPort = "3001"
 
 serverAddr :: String
 serverAddr = "192.168.0.14"
@@ -67,20 +111,21 @@ asClient = withSocketsDo . bracket (socketToAddress serverAddr serverPort) (\s -
 -- Connect a socket to a remote address
 socketToAddress :: HostName -> ServiceName -> IO Socket
 socketToAddress toAddress toPort = do
-    (serverAddrInfo:_) <- getAddrInfo Nothing (Just toAddress) (Just toPort)
-    s <- socket (addrFamily serverAddrInfo) Datagram defaultProtocol
-    connect s (addrAddress serverAddrInfo)
-    return s
+  (serverAddrInfo:_) <- getAddrInfo Nothing (Just toAddress) (Just toPort)
+  s <- socket (addrFamily serverAddrInfo) Datagram defaultProtocol
+  -- Connect once so we can use send rather than sendTo
+  connect s (addrAddress serverAddrInfo)
+  return s
 
 -- | Create a socket than can be listened to
 listenSocket :: ServiceName -> IO Socket
 listenSocket listenPort = do
-    -- Configure for accepting connections
-    let hints = Just $ defaultHints { addrFlags = [AI_PASSIVE] }
-    (serverAddrInfo:_) <- getAddrInfo hints Nothing (Just listenPort)
-    sock <- socket (addrFamily serverAddrInfo) Datagram defaultProtocol
-    bindSocket sock (addrAddress serverAddrInfo)
-    return sock
+  -- Configure for accepting connections
+  let hints = Just $ defaultHints { addrFlags = [AI_PASSIVE] }
+  (serverAddrInfo:_) <- getAddrInfo hints Nothing (Just listenPort)
+  sock <- socket (addrFamily serverAddrInfo) Datagram defaultProtocol
+  bindSocket sock (addrAddress serverAddrInfo)
+  return sock
 
 -- | Send a 'Binary' value to a socket
 sendB :: (MonadIO m, Binary a) => Socket -> a -> m Int
